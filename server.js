7259774bf38a15bdba053c02b1570b81c72cf188
FlixDerPartyBus <felixriemer00@gmail.com>
2019-03-26 17:39:21 +0100

    onoff LED versuche in server.js

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+// });
+
+var Gpio = require('onoff').Gpio;
+var LED = new Gpio(4, 'out');
+var blinkInterval = setInterval(blinkLED, 250);
+
+function blinkLED() {
+  if( LED.readSync() === 0) {
+    LED.writeSync(1);
+  } else {
+    LED.writeSync(0);
+  }
+}
+
+function endBlink() {
+  clearInterval(blinkInterval);
+  LED.writeSync(0);
+  LED.unexport();
+}
+
+setTimeout(endBlink, 5000);
