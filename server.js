a4dcf9b0ad1893ba66f39fbdd3fdf79fb88c8e04
FlixDerPartyBus <felixriemer00@gmail.com>
2019-03-26 17:44:55 +0100

    weiter testen

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-// var express = require('express');
-// var app = express();
+var express = require('express');
+var app = express();
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!');
-// });
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
 
 var Gpio = require('onoff').Gpio;
 var LED = new Gpio(4, 'out');
